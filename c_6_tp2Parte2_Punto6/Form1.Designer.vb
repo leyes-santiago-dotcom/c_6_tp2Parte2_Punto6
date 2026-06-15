511d86c1d4e5f86084813332cde0df992e99f1d2
leyes-santiago-dotcom <leyessantifacultad@gmail.com>
2026-06-15 00:36:30 -0300

    finalizacion del programa

--- c_6_tp2Parte2_Punto6/Form1.Designer.vb
+++ c_6_tp2Parte2_Punto6/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         cmdEjecutar = New Button()
         lstMuestra = New ListBox()
+        lblMuestra = New Label()
         SuspendLayout()
         ' 
         ' cmdEjecutar
@@ -38,24 +39,36 @@ Partial Class Form1
         ' lstMuestra
         ' 
         lstMuestra.FormattingEnabled = True
-        lstMuestra.Location = New Point(112, 12)
+        lstMuestra.Location = New Point(12, 67)
         lstMuestra.Name = "lstMuestra"
-        lstMuestra.Size = New Size(150, 444)
+        lstMuestra.Size = New Size(150, 384)
         lstMuestra.TabIndex = 1
+        ' 
+        ' lblMuestra
+        ' 
+        lblMuestra.AutoSize = True
+        lblMuestra.Location = New Point(12, 44)
+        lblMuestra.Name = "lblMuestra"
+        lblMuestra.Size = New Size(62, 20)
+        lblMuestra.TabIndex = 2
+        lblMuestra.Text = "Muestra"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(284, 450)
+        ClientSize = New Size(278, 450)
+        Controls.Add(lblMuestra)
         Controls.Add(lstMuestra)
         Controls.Add(cmdEjecutar)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents cmdEjecutar As Button
     Friend WithEvents lstMuestra As ListBox
+    Friend WithEvents lblMuestra As Label
 
 End Class
--- c_6_tp2Parte2_Punto6/Form1.vb
+++ c_6_tp2Parte2_Punto6/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    '6. Determinar todos los números capicúas de 3 cifras. 
+    Dim Numero, Unidad, Centena As Integer
 
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        For Numero = 100 To 999 Step 1
+            Centena = (Numero \ 100)
+            Unidad = (Numero Mod 10)
+            If Centena = Unidad Then
+                lstMuestra.Text = Numero
+
+            End If
+
+
+        Next
+    End Sub
 End Class
